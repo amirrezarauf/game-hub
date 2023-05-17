64b8186ad67488d75316cecd0d09ebde4d071dea
Amir Rauf <amirrrauf@gmail.com>
2023-05-17 11:31:27 +0330

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>("/games");
@@ -29,5 +29,6 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000, //24h
   });
 export default useGames;
