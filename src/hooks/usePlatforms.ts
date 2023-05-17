8076a41c0c3dd178fc268a9a3f9dd5e782e5db42
Amir Rauf <amirrrauf@gmail.com>
2023-05-17 12:34:46 +0330

    Refactor- create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24 hrs
-    initialData: { count: platforms.length, results: platforms },
+    initialData: platforms,
   });
 
 export default usePlatforms;
