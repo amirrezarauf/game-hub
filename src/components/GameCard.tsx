00dbe5e75f69c784e9900f7ea292e4e7b4bea9c8
Amir Rauf <amirrrauf@gmail.com>
2023-05-26 23:31:23 +0330

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -20,7 +21,8 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
+          <Link to={"/games/" + game.slug}>{game.name}</Link>{" "}
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const { data: game, isLoading, error } = useGame(slug!);
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
